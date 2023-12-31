544e95eefc810a91c64ac6063091ecc8d1e15e0d
ssmdbayzid <ssmdbayzid@gmail.com>
2023-12-31 09:26:27 +0600

    tailwind added

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <h1 className=" text-red-600 font-bold underline">
+    Hello world!
+  </h1>
   )
 }
 
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Header from '../component/Header'
 
 const MainLayout = () => {
   
     return (
     <div>
+        <Header />
         <Outlet />
     </div>
   )
